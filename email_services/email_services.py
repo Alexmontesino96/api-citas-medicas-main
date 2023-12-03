dfc7ce1da03980bc40a56b0b6499cb93b71becf4
Alexmontesino96 <alexmontesinocastro9@gmail.com>
2023-12-02 22:57:38 -0500

    Se anadio el servicio email para enviar codigo de confirmation para implementar el 2FA en el login

---
+++ email_services/email_services.py
@@ -0,0 +1,58 @@
+# Crear el mensaje del correo electrónico
+import smtplib
+from dotenv import load_dotenv
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText
+import os
+from pydantic import EmailStr
+from fastapi import HTTPException
+from random import randint
+
+
+
+def send_email_confirmation_code(addressee: EmailStr):
+    # Cargar las variables de entorno
+    try:
+        load_dotenv()
+        remitente = os.getenv("USER_THERAPY")
+        password = os.getenv("PASS")
+        temporary_code = generate_confirmation_code()
+        print(f'{remitente} {password}')
+
+        # Crea el mensaje
+        msg = MIMEMultipart()
+        msg['Subject'] = 'Confirmation code'
+        msg['From'] = remitente
+        msg['To'] = addressee
+
+        with open('/Users/alexmontesino/Documents/GitHub/api-citas-medicas-main/email_services/email_confirmation_code_html.html', 'r') as html:
+            html = html.read()
+            # Reemplazar el marcador de posición con el código generado
+        html = html.replace("123456", str(temporary_code))
+        
+        # Adjunta el mensaje al objeto MIMEMultipart
+        msg.attach(MIMEText(html, 'html'))
+
+        # Crea el servidor
+        server = smtplib.SMTP('smtp.gmail.com: 587')
+        server.starttls()
+        server.login(remitente, password)
+        server.sendmail(remitente, addressee, msg.as_string())
+        server.quit()
+
+        print(temporary_code)
+        return temporary_code
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error -> {e}")
+    
+
+
+def generate_confirmation_code():
+    """
+    Genera un código de confirmación de 6 dígitos.
+
+    Returns:
+        int: Código de confirmación.
+    """
+    return randint(100000, 999999)
